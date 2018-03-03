10f7bc9d417d1273df905d696090315e5ae8f37d
fussraider <fussraider@gmail.com>
2018-03-03 04:54:14 +0300

    проверка конфига

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 namespace App;
-require __DIR__.'/app/app.php';
 
+if(!file_exists(__DIR__.'/app/config.php')){
+    die('Не найден файл конфигурации. Для первоначальной настройки необходимо скопировать/переменовать файл '.__DIR__.'/app/config.example.php с именем config.php и заполнить необходимыми данными.');
+}
+
+require __DIR__.'/app/app.php';
 $app = new App;
 
 ?>
